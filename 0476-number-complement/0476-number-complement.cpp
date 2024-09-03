4c787aef2add82ee34cfbdcfcc1beb7018b479ec
Mohit-Pandey1 <123255545+Mohit-Pandey1@users.noreply.github.com>
2024-09-03 12:32:41 +0530

    Time: 2 ms (48.13%), Space: 7.6 MB (46.99%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int ans = 0;
+        int i = 0;
+        while(num){
+            (num % 2 == 1) ?: ans += pow(2,i);
+            num/=2;
+            i++;
+        }
+        return ans;
+    }
+};
